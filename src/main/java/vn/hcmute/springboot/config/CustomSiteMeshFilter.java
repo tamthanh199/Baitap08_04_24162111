66581a410bdaf95b889ad6d432ab6dd6fb2bfa5b
Doan Thanh Tam <doanthanhtam1909@gmail.com>
2026-09-17 14:33:11 +0700

    Add REST API and Swagger for Category and Product

--- src/main/java/vn/hcmute/springboot/config/CustomSiteMeshFilter.java
+++ src/main/java/vn/hcmute/springboot/config/CustomSiteMeshFilter.java
@@ -7,15 +7,27 @@ public class CustomSiteMeshFilter extends ConfigurableSiteMeshFilter {
 
     @Override
     protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+
         builder
             .addDecoratorPath("/*", "web.jsp")
             .addDecoratorPath("/admin/*", "admin.jsp")
+
             .addExcludedPath("/image*")
             .addExcludedPath("/product-image*")
             .addExcludedPath("/profile-image*")
+
             .addExcludedPath("/static/*")
             .addExcludedPath("/static/**")
+
             .addExcludedPath("/api/*")
-            .addExcludedPath("/api/**");
+            .addExcludedPath("/api/**")
+
+            .addExcludedPath("/swagger-ui.html")
+            .addExcludedPath("/swagger-ui/*")
+            .addExcludedPath("/swagger-ui/**")
+
+            .addExcludedPath("/v3/api-docs")
+            .addExcludedPath("/v3/api-docs/*")
+            .addExcludedPath("/v3/api-docs/**");
     }
 }
